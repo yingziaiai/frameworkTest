df78b27bd6a2f0a382a047c26501ec43ac96955b
yingziaiai <06fuping@163.com>
2016-12-12 16:43:47 +0800

    add the logic for JSTL test

---
+++ src/main/java/com/spring/annotationTest/AnnotationTest.java
@@ -0,0 +1,6 @@
+package com.spring.annotationTest;
+//  Spring注解详解 http://blog.csdn.net/xyh820/article/details/7303330/
+//http://docs.spring.io/spring/docs/4.2.0.BUILD-SNAPSHOT/spring-framework-reference/htmlsingle/#orm-hibernate
+public class AnnotationTest {
+
+}
